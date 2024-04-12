21e0750c1b4ccfba7a827f9dd056543f7be70bf6
jrfm <moesch.jonas@protonmail.com>
2024-04-12 17:13:16 +0200

    Test hinzufügen

--- src/stabwerkmodelle/mats_beton.py
+++ src/stabwerkmodelle/mats_beton.py
@@ -1,12 +1,21 @@
 def f_cd(alpha_cc, f_ck_cyl, gamma_c=1.5):
     """
-    Berechnung des Bemessungswertes der Betondruckfestigkeit nach DIN EN 1992-1-1, 3.1.6
-    :param f_ck_cyl: charakteristischer Zylinderdruckfestigkeitswert des Betons
-    :param alpha_cc: Korrekturfaktor für langzeitige Einwirkungen
-    :return: Bemessungswert der Betondruckfestigkeit
+    Berechnung des Bemessungswertes der Betondruckfestigkeit nach DIN EN 1992-1-1, 3.1.6 (1)
+    Examples:
+        >>> f_cd(0.85, 30, 1.5)
+        17.0
+
+    
+    Args:
+    alpah_cc: Korrekturfaktor für langzeitige Einwirkungen
+    f_ck_cyl: charakteristischer Zylinderdruckfestigkeitswert des Betons
+    gamma_c: Teilsicherheitsbeiwert für Beton
+
+    Returns: Bemessungswert der Betondruckfestigkeit f_cd
     """ 
     return alpha_cc * (f_ck_cyl/ gamma_c)
 
 
 def hello():
     print("Hello from .py")
+    return "Hello from .py"
