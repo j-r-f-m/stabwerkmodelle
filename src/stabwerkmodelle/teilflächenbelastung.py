5167f9be0361a1dcc4d2dc2b12407f3c312a07e7
jrfm <moesch.jonas@protonmail.com>
2024-04-13 14:02:50 +0200

    Füge Ac= hinzu

--- src/stabwerkmodelle/teilflächenbelastung.py
+++ src/stabwerkmodelle/teilflächenbelastung.py
@@ -4,12 +4,14 @@ Modul zur Berechnung der Teilfächenbelastung
 
 #  Reduktion der Belastungsfläche bei ausmittiger Lasteintragung
 
+
 def b1_red(b1, e):
     """
     Berechnet den reduzierten Wert von b1_red, bei ausmittiger Lasteintragung.
+    Nach EC 2 - 2012, 6.7, S.98
 
     Parameter:
-    b1 (float): Breite der Lasteintragsfläche (z. Bsp. Die Seite b eines Elastomerlagers).
+    b1 (float): Seitenlänge b1 der Lasteintragsfläche (z. Bsp. Die Seite b eines Elastomerlagers).
     e (float): Der Wert der Exentrizität.
 
     Returns:
@@ -18,14 +20,42 @@ def b1_red(b1, e):
     return b1 - 2 * e
 
 
-def b2_red(b1_red):
+def b2(b1):
     """
-    Berechnet den Wert von b2_red basierend auf dem gegebenen Wert von b1_red.
+    Berechnet die Seitenlänge b2 der Verteilungsfläche Ac1.
+    Nach EC 2 - 2012, 6.7, S.98
 
     Parameter:
-    b1_red (float): Der Wert von b1_red.
+    b1 (float): Seitenlänge d1 der Lasteintragsfläche Ac0.
 
     Returns:
-    float: Der berechnete Wert von b2_red.
+    float: Der berechnete Wert von b2.
     """
-    return 3 * b1_red
+    return 3 * b1
+
+def d2(d1):
+    """
+    Berechnet die Seitenlänge d2 der Verteilungsfläche Ac1.
+    Nach EC 2 - 2012, 6.7, S.98
+
+    Parameter:
+    d1 (float): Seitenlänge d1 der Lasteintragsfläche Ac0.
+    Returns:
+    float: Der berechnete Wert von d2.
+    """
+    return 3 * d1
+
+
+def A_c0(b_1, d_1):
+    """
+    Berechnet die Belastungsfläche A_c0 
+    nach EC 2 - 2012, 6.7, S. 98.
+
+    Parameter:
+    b_1 (float): Die Länge der Basis des Rechtecks.
+    d_1 (float): Die Höhe des Rechtecks.
+
+    Returns:
+    float: Die Fläche des Rechtecks.
+    """
+    return b_1 * d_1
--- tests/teilflächenbelastung_test.py
+++ tests/teilflächenbelastung_test.py
@@ -11,17 +11,27 @@ def test_b1_red():
     """
     Testfall für die Funktion b1_red im Modul teilflächenbelastung.
 
-    Dieser Testfall überprüft, ob die Funktion b1_red den erwarteten Wert zurückgibt, 
+    Dieser Testfall überprüft, ob die Funktion b1_red den erwarteten Wert zurückgibt,
     wenn die Eingabewerte 0.2 und 0.05 sind. Der erwartete Ausgabewert ist 0.1.
     """
     assert teilflächenbelastung.b1_red(0.2, 0.05) == 0.1
 
+
 def test_b2_red():
     """
-    Test case for the b2_red function in teilflächenbelastung module.
-    
-    This test checks if the b2_red function returns the expected value when given an input of 0.1.
-    The expected output is 0.3, and the function is expected to round the result to 2 decimal places.
-    """
-    assert round(teilflächenbelastung.b2_red(0.1), 2) == 0.3
+    Testfall für die Funktion b2_red im Modul teilflächenbelastung.
 
+    Dieser Testfall überprüft, ob die Funktion b2_red den erwarteten Wert zurückgibt,
+    wenn der Eingabewert 0.1 ist. Der erwartete Ausgabewert ist 0.3 und die Funktion soll das Ergebnis auf 2 Dezimalstellen runden.
+    """
+    assert round(teilflächenbelastung.b2(0.1), 2) == 0.3
+
+
+def test_A_co():
+    """
+    Testfall für die Funktion A_co im Modul teilflächenbelastung.
+
+    Dieser Testfall überprüft, ob die Funktion A_co den erwarteten Wert zurückgibt,
+    wenn die Eingabewerte 0.2 und 0.1 sind. Der erwartete Ausgabewert ist 0.02 und die Funktion soll das Ergebnis auf 2 Dezimalstellen runden.
+    """
+    assert round(teilflächenbelastung.A_c0(0.2, 0.1), 2) == 0.02
